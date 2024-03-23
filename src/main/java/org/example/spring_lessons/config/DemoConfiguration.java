ef9e327366a11a5c844af7c7372502599b1ee9ec
Andrew Kurylyk <andronniy7@gmail.com>
2024-03-23 21:06:06 +0200

    First

--- src/main/java/org/example/spring_lessons/config/DemoConfiguration.java
+++ src/main/java/org/example/spring_lessons/config/DemoConfiguration.java
@@ -1,7 +1,9 @@
 package org.example.spring_lessons.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DemoConfiguration {
 
     @Bean
--- src/main/java/org/example/spring_lessons/controller/ProductController.java
+++ src/main/java/org/example/spring_lessons/controller/ProductController.java
@@ -1,5 +1,6 @@
 package org.example.spring_lessons.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,18 @@ import java.util.List;
 
 @RestController
 public class ProductController {
+
+
     private String testProductName;
 
     @GetMapping("/products")
 
     public ResponseEntity<List<String>> getProducts() {
         return ResponseEntity.ok(List.of(testProductName));
+
+    }
+    @Autowired
+    public void setTestProductName(String testProductName) {
+        this.testProductName = testProductName;
     }
 }
